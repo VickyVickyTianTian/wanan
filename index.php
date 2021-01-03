17f8768606ea38792b80dfb5907bedaad8403202
Jack Lee <li8336818@gmail.com>
2021-01-03 22:05:56 +1300

    polish code

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $password = $_SESSION['password'];
 if ($email && $password) {
     $sql = "SELECT * FROM users WHERE email = '$email'";
     $run_Sql = mysqli_query($con, $sql);
-    if($run_Sql){
+    if($run_Sql) {
         $fetch_info = mysqli_fetch_assoc($run_Sql);
         $status = $fetch_info['status'];
         $code = $fetch_info['code'];
