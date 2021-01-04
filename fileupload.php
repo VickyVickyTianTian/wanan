dc6d0537617b6e12348a93ab88ef622ffcf00327
Jack Lee <li8336818@gmail.com>
2021-01-04 14:37:08 +1300

    polish code

--- fileupload.php
+++ fileupload.php
@@ -21,14 +21,29 @@ function upload()
     $baseURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . getenv('BASE_DOMAIN');
 
     $total = count($_FILES[$input]['name']);
-    $path = __DIR__ . '/storage/';
+
+    // make sure the directory `storage/[YYYY-mm-dd]` exists
+    $path = __DIR__ . '/storage/' . date('Y-m-d');
+    if (!mkdir($path) && !is_dir($path)) {
+        return ['error' => 'Oh snap! We could not upload the file now. Please try again later.'];
+    }
+
     for ($i = 0; $i < $total; $i++) {
         $tmpFilePath = $_FILES[$input]['tmp_name'][$i]; // the temp file path
-        $fileName = $_FILES[$input]['name'][$i]; // the file name
+        $oldFileName = $_FILES[$input]['name'][$i]; // the file name
         $fileSize = $_FILES[$input]['size'][$i]; // the file size
 
+        // only jpg, jpeg, png, pdf files are allowed to upload
+        $ext = pathinfo($oldFileName, PATHINFO_EXTENSION);
+        if (!in_array($ext, ['jpg', 'jpeg', 'png', 'pdf'])) {
+            return ['error' => 'Please upload .jpg, .jpeg, .png, .pdf files.'];
+        }
+
+        // generate new fileName
+        $fileName = md5($oldFileName . microtime(true)) . '.' . $ext;
+
         if ($tmpFilePath) {
-            $newFilePath = $path . $fileName;
+            $newFilePath = $path . '/' . $fileName;
             $newFileUrl = $baseURL . '/storage/' . $fileName;
 
             if (move_uploaded_file($tmpFilePath, $newFilePath)) {
@@ -36,16 +51,16 @@ function upload()
                 $preview[] = $newFileUrl;
                 $config[] = [
                     'key' => $fileId,
-                    'caption' => $fileName,
+                    'caption' => $oldFileName,
                     'size' => $fileSize,
                     'downloadUrl' => $newFileUrl, // the url to download the file
                     'url' => $baseURL . '/filedelete.php', // server api to delete the file based on key
                 ];
             } else {
-                $errors[] = $fileName;
+                $errors[] = $oldFileName;
             }
         } else {
-            $errors[] = $fileName;
+            $errors[] = $oldFileName;
         }
     }
     $out = ['initialPreview' => $preview, 'initialPreviewConfig' => $config, 'initialPreviewAsData' => true];
